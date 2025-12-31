9955865e772d2dc9bb2e0e1bb25573f175737e4c
Diego Saavedra <dsaavedra88@gmail.com>
2025-12-31 01:39:50 -0500

    fix: Corregir sintaxis de LSP plugin
    - Corregido operador incorrecto '==' en configuración TypeScript
    - Eliminados múltiples errores de sintaxis
    - Simplificada configuración de servidores LSP
    - Usando vim.lsp.config directamente (sin lspconfig.lua_ls wrapper)
    - Servidores configurados: lua_ls, ts_ls, pyright, html, cssls, tailwindcss, dartls

--- nvim/lua/statick/plugins/lsp.lua
+++ nvim/lua/statick/plugins/lsp.lua
@@ -6,62 +6,74 @@ return {
     -- Habilitar capacidades para autocompletado
     local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-    -- Lua (para archivos de configuración nvim)
-    lspconfig.lua_ls = {
-      settings = {
-        Lua = {
-          diagnostics = { enable = false, globals = { "vim" } },
-          workspace = { checkThirdParty = false },
-        },
-      },
-    }
+    vim.lsp.config({
+      capabilities = capabilities,
 
-    -- TypeScript/JavaScript
-    lspconfig.ts_ls = {
-      settings = {
-        typescript = {
-          inlayHints = { enabled = true },
-        },
-      },
-    }
-
-    -- Python
-    lspconfig.pyright = {
-      settings = {
-        python = {
-          analysis = {
-            autoSearchPaths = true,
-            autoImportCompletions = true,
-            typeCheckingMode = "basic",
+      -- Lua (para archivos de configuración nvim)
+      lua_ls = {
+        settings = {
+          Lua = {
+            diagnostics = { enable = false, globals = { "vim" } },
           },
         },
       },
-    }
 
-    -- HTML
-    lspconfig.html = {}
-
-    -- CSS
-    lspconfig.cssls = {}
-
-    -- Tailwind CSS
-    lspconfig.tailwindcss = {
-      filetypes = { "css", "scss", "less" },
-    },
-
-    -- Dart (para Flutter)
-    lspconfig.dartls = {
-      cmd = { "dart", "language-server", "--protocol=lsp" },
-      filetypes = { "dart" },
-      root_dir = function(fname)
-        return require("lspconfig.util").root_pattern(".git", ".dart_tool")(fname) or vim.loop.cwd
-      end,
-      settings = {
-        dart = {
-          completeFunctionCalls = true,
-          showTodos = true,
+      -- TypeScript/JavaScript
+      ts_ls = {
+        settings = {
+          javascript = {
+            inlayHints = {
+              includeInlayParameterNameHints = "all",
+              includeInlayFunctionParameterTypeHints = true,
+            },
+          },
+          typescript = {
+            inlayHints = {
+              includeInlayParameterNameHints = "all",
+              includeInlayFunctionParameterTypeHints = true,
+            },
+          },
         },
       },
-    },
-  },
+
+      -- Python
+      pyright = {
+        settings = {
+          python = {
+            analysis = {
+              autoSearchPaths = true,
+              autoImportCompletions = true,
+              typeCheckingMode = "basic",
+            },
+          },
+        },
+      },
+
+      -- HTML
+      html = {},
+
+      -- CSS
+      cssls = {},
+
+      -- Tailwind CSS
+      tailwindcss = {
+        settings = {},
+      },
+
+      -- Dart (para Flutter)
+      dartls = {
+        cmd = { "dart", "language-server", "--protocol=lsp" },
+        filetypes = { "dart" },
+        root_dir = function(fname)
+          return require("lspconfig.util").root_pattern(".git")(fname) or vim.loop.cwd
+        end,
+        settings = {
+          dart = {
+            completeFunctionCalls = true,
+            showTodos = true,
+          },
+        },
+      },
+    })
+  end,
 }
