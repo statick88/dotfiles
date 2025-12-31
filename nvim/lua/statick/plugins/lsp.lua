012b41073882b5649da3fcef96ed360ba249cde1
Diego Saavedra <dsaavedra88@gmail.com>
2025-12-31 01:41:41 -0500

    fix: Reescribir lsp.lua para usar lspconfig directamente
    - Simplificar configuración LSP
    - Eliminado errores de vim.lspconfig
    - Usar lspconfig.ts_ls() con configuración JavaScript/TypeScript
    - Configurar servidores: lua_ls, ts_ls, pyright, html, cssls, tailwindcss, dartls
    - Estructura correcta de lspconfig
    - Sin dependencias complejas de Mason-lspconfig para configuraciones avanzadas

--- nvim/lua/statick/plugins/lsp.lua
+++ nvim/lua/statick/plugins/lsp.lua
@@ -1,79 +1,129 @@
--- LSP (Language Server Protocol): Configuración simplificada y robusta
--- Convierte Neovim en un IDE completo sin dependencias complejas
+-- LSP (Language Server Protocol): Configuración de servidores LSP
+-- Lenguajes configurados: Lua, TypeScript/JavaScript, Python, HTML, CSS, Tailwind, Dart
 return {
   "neovim/nvim-lspconfig",
+  dependencies = {
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "hrsh7th/cmp-nvim-lsp",
+  },
   config = function()
-    -- Habilitar capacidades para autocompletado
+    -- Inicializar Mason (gestor de herramientas de desarrollo)
+    require("mason").setup()
+    local mason_lspconfig = require("mason-lspconfig")
+
+    -- Configurar qué servidores LSP instalar automáticamente
+    mason_lspconfig.setup({
+      ensure_installed = {
+        "lua_ls",
+        "ts_ls",
+        "pyright",
+        "html",
+        "cssls",
+        "tailwindcss",
+        "dartls",
+      },
+      automatic_installation = true,
+    })
+
+    -- Habilitar capacidades avanzadas para autocompletado
     local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-    vim.lsp.config({
-      capabilities = capabilities,
+    -- Configurar servidores LSP
+    local lspconfig = require("lspconfig")
 
-      -- Lua (para archivos de configuración nvim)
-      lua_ls = {
-        settings = {
-          Lua = {
-            diagnostics = { enable = false, globals = { "vim" } },
+    -- Lua (para archivos de configuración nvim)
+    lspconfig.lua_ls({
+      settings = {
+        Lua = {
+          diagnostics = {
+            enable = false,
+            globals = { "vim" },
           },
-        },
-      },
-
-      -- TypeScript/JavaScript
-      ts_ls = {
-        settings = {
-          javascript = {
-            inlayHints = {
-              includeInlayParameterNameHints = "all",
-              includeInlayFunctionParameterTypeHints = true,
-            },
-          },
-          typescript = {
-            inlayHints = {
-              includeInlayParameterNameHints = "all",
-              includeInlayFunctionParameterTypeHints = true,
-            },
-          },
-        },
-      },
-
-      -- Python
-      pyright = {
-        settings = {
-          python = {
-            analysis = {
-              autoSearchPaths = true,
-              autoImportCompletions = true,
-              typeCheckingMode = "basic",
-            },
-          },
-        },
-      },
-
-      -- HTML
-      html = {},
-
-      -- CSS
-      cssls = {},
-
-      -- Tailwind CSS
-      tailwindcss = {
-        settings = {},
-      },
-
-      -- Dart (para Flutter)
-      dartls = {
-        cmd = { "dart", "language-server", "--protocol=lsp" },
-        filetypes = { "dart" },
-        root_dir = function(fname)
-          return require("lspconfig.util").root_pattern(".git")(fname) or vim.loop.cwd
-        end,
-        settings = {
-          dart = {
-            completeFunctionCalls = true,
-            showTodos = true,
+          workspace = {
+            checkThirdParty = false,
           },
         },
       },
     })
+
+    -- TypeScript/JavaScript (configuración para ts_ls)
+    lspconfig.ts_ls({
+      settings = {
+        javascript = {
+          inlayHints = {
+            includeInlayParameterNameHints = "all",
+            includeInlayFunctionParameterTypeHints = true,
+            includeInlayVariableTypeHints = true,
+            includeInlayPropertyDeclarationTypeHints = true,
+          },
+        },
+        },
+      },
+    })
+
+    -- Python (configuración para pyright)
+    lspconfig.pyright({
+      settings = {
+        python = {
+          analysis = {
+            autoSearchPaths = true,
+            autoImportCompletions = true,
+            typeCheckingMode = "basic",
+            diagnosticSeverity = {
+              error = "Error",
+              warning = "Warning",
+              information = "Information",
+            },
+          },
+        },
+      },
+    })
+
+    -- HTML
+    lspconfig.html({})
+
+    -- CSS
+    lspconfig.cssls({})
+
+    -- Tailwind CSS
+    lspconfig.tailwindcss({
+      settings = {
+        tailwindCSS = {
+          experimental = {
+            classRegex = "class\\s*((?:[a-zA-Z]+[a-z0-9]*)+",
+          },
+        },
+      },
+    })
+
+    -- Dart/Flutter
+    lspconfig.dartls({
+      cmd = { "dart", "language-server", "--protocol=lsp" },
+      filetypes = { "dart" },
+      init_options = {
+        onlyAnalyzeProjectsWithOpenFiles = true,
+        suggestFromUnimportedLibraries = true,
+        closingLabels = true,
+        outline = true,
+      },
+      settings = {
+        dart = {
+          completeFunctionCalls = true,
+          showTodos = true,
+        },
+      },
+    })
+
+    -- Keymaps de LSP
+    local keymap = vim.keymap
+
+    keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+    keymap.set("n", "gr", vim.lsp.buf.references, { desc = "Go to references" })
+    keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover documentation" })
+    keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Next diagnostic" })
+    keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Previous diagnostic" })
+    keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename" })
+    keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
   end,
 }
